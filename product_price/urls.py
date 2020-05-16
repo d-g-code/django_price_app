96669a880003ea6a2c19d58156fc482a0521e327
dawnik <qsmiespa@wp.pl>
2020-05-17 01:52:58 +0200

    create view sing up way for users

--- product_price/urls.py
+++ product_price/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'product_price'
 urlpatterns = [
     path('', views.index_product, name='index_product'),
+    path('signup', views.signup, name='signup'),
 ]
--- product_price/views.py
+++ product_price/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
 from .models import Product
-from django.http import Http404
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index_product(request):
     all_products = Product.objects.all()
     return render(request, 'product_price/product.html', {'all_products': all_products})
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return render(request, 'product_price/product.html')
+    else:
+        form = UserCreationForm()
+        return render(request, 'product_price/signup.html', {'form': form})
 
