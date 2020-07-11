583f3453cd3b0254d701a2d05c784e9e8af19956
dawnik <qsmiespa@wp.pl>
2020-07-12 01:09:32 +0200

    change models product from 30 char to 40

--- product_price/models.py
+++ product_price/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=40)
     price = models.DecimalField(max_digits=5, decimal_places=2, default='')
     store = models.CharField(max_length=20, default='')
 
