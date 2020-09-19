f0704d7e548fcfda02b7d0d21c4ecb8710d4fc79
dawnik <qsmiespa@wp.pl>
2020-09-19 04:57:00 +0200

    added models kind and store field in product

--- product_price/models.py
+++ product_price/models.py
@@ -8,10 +8,18 @@ class Store(models.Model):
         return self.store
 
 
+class Kind(models.Model):
+    kind = models.CharField(max_length=10, default='')
+
+    def __str__(self):
+        return self.kind
+
+
 class Product(models.Model):
     product = models.CharField(max_length=40)
     price = models.DecimalField(max_digits=5, decimal_places=2, default='')
-    product_store = models.CharField(max_length=40, default='')
+    product_kind = models.ForeignKey(Kind, on_delete=models.CASCADE)
+    product_store = models.ForeignKey(Store, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.product
