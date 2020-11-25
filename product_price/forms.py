3a940e67e0fc319b33392e5533ec311de6bfc26b
d-g-code <>
2020-11-25 19:22:47 +0100

    change form added widget, message propmt top screan and red and style body change background color

--- product_price/forms.py
+++ product_price/forms.py
@@ -1,8 +1,14 @@
 from django import forms
-from .models import Product, Store
+from .models import Product, Store, Kind
 
 
 class AddProductForms(forms.ModelForm):
+    product = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-group', 'placeholder': 'product'}))
+    price = forms.DecimalField(widget=forms.TextInput(
+        attrs={'class': 'form-group', 'placeholder': 'price', 'type': 'number', 'step': 0.01, 'min': 0}))
+    product_kind = forms.ModelChoiceField(queryset=Kind.objects.all())
+    product_store = forms.ModelChoiceField(queryset=Store.objects.all())
+
     class Meta:
         model = Product
         fields = ['product', 'price', 'product_kind', 'product_store']
--- product_price/models.py
+++ product_price/models.py
@@ -19,7 +19,7 @@ class Product(models.Model):
     product = models.CharField(max_length=40, default='')
     price = models.DecimalField(max_digits=5, decimal_places=2, default='')
     product_kind = models.ForeignKey(Kind, on_delete=models.CASCADE)
-    product_store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    product_store = models.ForeignKey(Store, on_delete=models.  CASCADE)
 
     def __str__(self):
         return self.product
