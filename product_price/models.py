c4d6a91c82626ea8a7ca460ea12c19bfeb91045f
d-g-code <>
2020-12-30 16:56:57 +0100

    locally error. Validator to DecimalField in models set max_digits=5 and decimal_laces=2 so 'quantize result has too many digits for current context', max_digits must be greater than or equal decimal_places

--- product_price/models.py
+++ product_price/models.py
@@ -17,7 +17,7 @@ class Kind(models.Model):
 
 class Product(models.Model):
     product = models.CharField(max_length=40, default='')
-    price = models.DecimalField(max_digits=5, decimal_places=2, default='')
+    price = models.DecimalField(max_digits=7, decimal_places=2, default='')
     product_kind = models.ForeignKey(Kind, on_delete=models.CASCADE)
     product_store = models.ForeignKey(Store, on_delete=models.  CASCADE)
 
