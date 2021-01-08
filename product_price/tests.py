43e90a9405b16cb66b87bdb525a71f5253acb77b
d-g-code <>
2021-01-08 11:35:11 +0100

    test length name store and load page code

--- product_price/tests.py
+++ product_price/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Store
 
-# Create your tests here.
+
+class StoreModelTest(TestCase):
+
+    def test_name_representation(self):
+        # """max_length return False for name length 11 sign"""
+        name_store = Store(store='abcdefghij')
+        self.assertEqual(str(name_store), name_store.store)
+
+
+class ProjectTest(TestCase):
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
